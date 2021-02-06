63a2b5b7ee3bdcceb86a224be6916933973a74da
Sunil Pai <sunil8627@gmail.com>
2021-02-06 15:52:22 +0530

    Updated seeds file to create a few users and shared locations

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user1 = User.create(email: "user1@test.com", password: "123456", username: "User1")
+user2 = User.create(email: "user2@test.com", password: "123456", username: "User2")
+user3 = User.create(email: "user3@test.com", password: "123456", username: "User3")
+
+
+Place.create(:sender_id => user1.id, :receiver_id => user2.id, :place => "New York", :is_public => true)
+Place.create(:sender_id => user1.id, :receiver_id => user3.id, :place => "London", :is_public => true)
+Place.create(:sender_id => user1.id, :receiver_id => user2.id, :place => "Delhi", :is_public => false)
+
+Place.create(:sender_id => user2.id, :receiver_id => user1.id, :place => "Eiffel Tower", :is_public => true)
+Place.create(:sender_id => user2.id, :receiver_id => user1.id, :place => "Mumbai", :is_public => false)
+Place.create(:sender_id => user2.id, :receiver_id => user3.id, :place => "Cape Town", :is_public => true)
+
+Place.create(:sender_id => user3.id, :receiver_id => user1.id, :place => "Egypt", :is_public => true)
+Place.create(:sender_id => user3.id, :receiver_id => user1.id, :place => "Lahore", :is_public => false)
+Place.create(:sender_id => user3.id, :receiver_id => user2.id, :place => "Mexico", :is_public => true)
